26b2d029155f192c573cecfc7a44fdfe259e56ba
Leonides Lemus <xxluckyleoxx1@gmail.com>
2021-06-22 17:57:38 -0400

    leo branch

---
+++ src/components/UpdateProfilePage.js
@@ -0,0 +1,54 @@
+
+// import React from 'react'
+// import { Form, Button, Segment, Header, Message } from 'semantic-ui-react'
+
+
+// const UpdateProfilePage = props => {
+
+//     const [checked, setChecked] = useState(false)
+
+    
+    
+//     return (
+//         <div className= "update-profile-page">
+//             <div className= "top-bar">
+//                     <h1 className= "title">My Modium</h1>                    
+//             </div>
+//             <div className= "update-profile-page-body">
+                
+//                 <Header as='h2'>Customize Your Profile</Header>
+//                 <Form>
+//                     <Form.Field>
+//                         <label>Genres</label>
+//                         <Checkbox
+//                             label='Check this box'
+//                             onChange={this.toggle}
+//                             checked={this.state.checked}
+//                         />
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>Media Type</label>
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>Platforms</label>
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>Bio</label>
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <label>Profile Picture URL</label>
+//                     </Form.Field>
+//                     <Form.Field>
+//                         <Button type= 'submit'>Submit</Button>
+//                     </Form.Field>
+//                 </Form>
+//             </div>
+//         </div>
+//     )
+    
+    
+// }
+
+
+
+// export default UpdateProfilePage
